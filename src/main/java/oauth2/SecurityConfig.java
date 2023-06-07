247296112c381381d27b610baee459e50a91a3e5
Sol <sodanabe@gmail.com>
2023-06-08 00:36:59 +0300

    Initial commit

---
+++ src/main/java/oauth2/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/favicon.ico").permitAll();
+                    auth.anyRequest().authenticated(); }
+                )
+                .oauth2Login(withDefaults())
+                //.formLogin(withDefaults())
+                .build();
+    }
+
+}
